a604d0ea7cc0d4a7d5cf6893ab65adf5b03f5d1d
MissUsagi <syljanko@gmail.com>
2024-09-13 18:18:11 +0200

    feat: category tabs created

---
+++ zadanie-rekrutacyjne/src/components/ui/CategoryTabs.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from "vue";
+const props = defineProps({
+  tabs: {
+    type: Array,
+    required: true,
+  },
+});
+
+const selectedTabIndex = ref(0);
+
+function setSelectedTab(index) {
+  selectedTabIndex.value = index;
+}
+</script>
+
+<template>
+  <div class="category-tabs">
+    <span v-for="(tab, index) in tabs" :key="tab" class="tab" :class="{ 'tab-active': selectedTabIndex === index }" @click="setSelectedTab(index)">{{
+      tab
+    }}</span>
+  </div>
+</template>
+
+<style scoped>
+.category-tabs {
+  @apply flex items-center gap-6 sm:gap-12 font-flex text-sm xs:text-base;
+}
+
+.tab {
+  @apply font-flex cursor-pointer;
+}
+
+.tab-active {
+  @apply font-semibold text-primary border-b border-primary;
+}
+</style>
